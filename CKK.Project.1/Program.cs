226e003f941f7fe666b447a471e112a76bf0ffcb
rylan312231 <branzrylan@gmail.com>
2022-05-16 17:15:37 -0600

    initial; push

---
+++ CKK.Project.1/Program.cs
@@ -0,0 +1,36 @@
+﻿using CKK.Project._1;
+using System;
+namespace CKK.Project
+{
+      public class program
+      {
+        static void Main(string[] args)
+        { }
+                public class StoreItem
+                {
+                    private Product _product;
+                    private int _quantity;
+
+                public StoreItem(Product product, int quantity)
+                {
+                        _product = product;
+                        _quantity = quantity;
+                }
+                public int GetQuantity()
+                {
+                        return _quantity;
+                }
+
+                public void SetQuantity(int quantity)
+                {
+                        _quantity = quantity;
+                }
+
+                public Product GetProduct()
+                {
+                        return _product;
+                }
+           }
+            
+      }
+}
